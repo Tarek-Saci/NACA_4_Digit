5242a790ef2ecd1cbbb81aa497a50540185e4ece
Tarek-Saci <tarek7saci@gmail.com>
2023-05-22 21:16:10 -0400

    Update NACA_Ploter.py

--- NACA_Ploter.py
+++ NACA_Ploter.py
@@ -3,22 +3,21 @@ import matplotlib as plt
 
 
 def generer_tableau_coor(N_point,xx,corde, type_distribution):
+    x_up
+    y_up
+    x_down
+    y_down 
     tepaisseur_pourcent = xx/100
     if type_distribution == 'lineaire':
         xc = np.linspace(0 , 1 , N_point)
     elif type_distribution == 'non-uniforme':
         tetha = np.linspace(0 , np.pi , N_point)
         xc = (1/2)*(1-np.cos(tetha))
-
-
     yt = 5 * tepaisseur_pourcent * (0.2969 * np.sqrt(xc) - 0.1260 * xc - 0.3516 * xc ** 2 + 0.2843 * xc ** 3 - 0.1036 * xc ** 4)
     x_up = xc * corde
     y_up = yt * corde
     x_down = xc * corde
     y_down = -yt * corde
-
-
-
     return
 def generer_coor_discretise():
     return
@@ -29,3 +28,13 @@ def pos_epaisseur_max():
 def graphique():
     return
 
+xx = int(input('NACA 00XX\n'))
+N_point = int(input('Nombre de points\n'))
+corde = int(input('corde\n'))
+type_distribution = input('type \n')
+
+generer_tableau_coor(N_point, xx,corde,type_distribution)
+
+
+print(f'x_up = {xc*corde}')
+print(f'y_up = {yt*corde}')
