69c75b2253f813a1daf3979da14aadbce809857d
Tarek-Saci <tarek7saci@gmail.com>
2023-05-22 21:40:57 -0400

    Update NACA_Ploter.py

--- NACA_Ploter.py
+++ NACA_Ploter.py
@@ -3,10 +3,10 @@ import matplotlib as plt
 
 
 def generer_tableau_coor(N_point,xx,corde, type_distribution):
-    x_up
-    y_up
-    x_down
-    y_down 
+    global x_up
+    global y_up
+    global x_down
+    global y_down
     tepaisseur_pourcent = xx/100
     if type_distribution == 'lineaire':
         xc = np.linspace(0 , 1 , N_point)
@@ -19,12 +19,16 @@ def generer_tableau_coor(N_point,xx,corde, type_distribution):
     x_down = xc * corde
     y_down = -yt * corde
     return
-def generer_coor_discretise():
-    return
-def epaisseur_max():
-    return
-def pos_epaisseur_max():
-    return
+
+def pos_epaisseur_max(x_up,y_up):
+    i = y_up.index(np.max(y_up)) #position du max de y_up dans le tableau
+    pos_epaisseur_max = x_up[i]
+    return pos_epaisseur_max
+
+epaisseur_max = lambda y_up : np.max(y_up)
+
+epaisseur_max = np.max(y_up)
+
 def graphique():
     return
 
@@ -36,5 +40,7 @@ type_distribution = input('type \n')
 generer_tableau_coor(N_point, xx,corde,type_distribution)
 
 
-print(f'x_up = {xc*corde}')
-print(f'y_up = {yt*corde}')
+print(f'x_up = {x_up}')
+print(f'y_up = {y_up}')
+print(f'x_down = {x_down}')
+print(f'y_down = {y_down}')
